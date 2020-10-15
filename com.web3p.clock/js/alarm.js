d911db79ce7deb10f9f4e24a4c746439a0d70946
erunapp <72909027+web3p-pt@users.noreply.github.com>
2020-10-16 04:57:29 +0900

    초기세팅

---
+++ com.web3p.clock/js/alarm.js
@@ -0,0 +1,31 @@
+;
+var data = {
+	    no: 1,
+	    eventid: "Test Event",
+	    eventon: "2020-06-25",
+	    alarmat: "09:00",
+	    period: "y",
+	    alarmfor: 3,
+	    alarmunit: "m",
+	    countgap: 10,
+	    countunit: "m",
+	    count: 5,
+	    volume: 9,
+	    vibrate: true,
+	    sound: "melody.mp3",
+	    message: "일어나요."
+	};
+
+function load() {
+    for (let id in objs) flds[id] = data[id];
+};
+
+function save() {	
+    for (let id in objs) data[id] = flds[id];
+};
+
+var form = window.eval(wdp.initForm());
+var objs = form.objs;
+var flds = form.flds;
+
+load();

89d85fd7eecf61f41577843c6178f020511d7190
erunapp <72942850+erunapp@users.noreply.github.com>
2020-10-20 03:24:43 +0900

    0.0.5

--- com.web3p.editor/js/main.js
+++ com.web3p.editor/js/main.js
@@ -1,28 +1,30 @@
 ;
-void (function (data) {
-	function tree(node) {
-		let files = [], folders = [];
+var container = document.getElementById("container");
+
+if (container.childElementCount == 0) {
+	void (function (data) {
+		let folder = document.getElementById('folder').innerHTML,
+			file = document.getElementById('file').innerHTML;
 		
-		for (key in node) {
-			if (typeof node[key] === 'number') {
-				files.push(file.replace('${name}', key).replace('${path}', node[key]));
-			} else {
-				folders.push(folder.replace('${name}', key).replace('${children}', tree(node[key])));
+		function tree(node) {
+			let html = [];
+			
+			for (key in node) {
+				if (typeof node[key] === 'number') {
+					html.push(file.replace('${name}', key).replace('${path}', node[key]));
+				} else {
+					html.push(folder.replace('${name}', key).replace('${children}', tree(node[key])));
+				}
 			}
+			return html.join('');
 		}
-		return folders.join('') + files.join('');
-	}
+		container.innerHTML = tree(data);
+	} (JSON.parse(wdp.getFiles())));
 
-	let folder = document.getElementById('folder').innerHTML,
-		file = document.getElementById('file').innerHTML;
-	
-	document.getElementById('container').innerHTML = tree(data);
-} (JSON.parse(wdp.getFiles())));
-
-document.getElementById("container")
-	.addEventListener('click', event => {
+	container.addEventListener('click', event => {
 		if (event.target.tagName == "BUTTON") {
 			let val = event.target.value;
 			location.href=`edit.html?file=${val}`;
 		}
 	});
+}

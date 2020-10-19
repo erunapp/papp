f48f7abc5d13566443ce7d8fa3b406bbfe9ecd67
erunapp <72942850+erunapp@users.noreply.github.com>
2020-10-20 04:40:09 +0900

    0.0.7

--- com.web3p.editor/js/main.js
+++ com.web3p.editor/js/main.js
@@ -1,33 +1,30 @@
 ;
 var container = document.getElementById("container");
-let state = wdp.loadState();
 
-if (state == "") {
-	void (function (data) {
-		let folder = document.getElementById('folder').innerHTML,
-			file = document.getElementById('file').innerHTML;
-		
-		function tree(node) {
-			let html = [];
-			
-			for (key in node) {
-				if (typeof node[key] === 'number') {
-					html.push(file.replace('${name}', key).replace('${path}', node[key]));
-				} else {
-					html.push(folder.replace('${name}', key).replace('${children}', tree(node[key])));
-				}
-			}
-			return html.join('');
-		}
-		container.innerHTML = tree(data);
-	} (JSON.parse(wdp.getFiles())));
+let folder = document.getElementById('folder').innerHTML,
+	file = document.getElementById('file').innerHTML;
 
-	container.addEventListener('click', event => {
-		if (event.target.tagName == "BUTTON") {
-			let val = event.target.value;
-			wdp.saveState(container.innerHTML);
-			location.href=`edit.html?file=${val}`;
+function tree(node) {
+	let html = [];
+
+	for (key in node) {
+		if (typeof node[key] === 'number') {
+			html.push(file.replace('${name}', key).replace('${path}', node[key]));
+		} else {
+			html.push(folder.replace('${name}', key).replace('${children}', tree(node[key])));
 		}
-	});
+	}
+	return html.join('');
 }
-else container.innerHTML = state;
+
+let state = wdp.loadState();
+container.innerHTML = (state == "") ? tree(JSON.parse(wdp.getFiles())) : state;
+
+container.addEventListener('click', event => {
+	if (event.target.tagName == "BUTTON") {
+		wdp.saveState(container.innerHTML);
+		
+		let val = event.target.value;
+		location.href=`edit.html?file=${val}`;
+	}
+});

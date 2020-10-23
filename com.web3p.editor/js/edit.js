f84f0c53d4eb45286e9022bfb34007a6af8e5c10
erunapp <72942850+erunapp@users.noreply.github.com>
2020-10-24 02:27:01 +0900

    0.0.9

--- com.web3p.editor/js/edit.js
+++ com.web3p.editor/js/edit.js
@@ -8,7 +8,7 @@ CodeMirror(document.getElementById('code'), {
     lineNumbers: true,
     tabSize: 4,
     value: wdp.getFile(file),
-    mode: 'javascript',
+    mode: 'htmlmixed',
     theme: 'cobalt'
   });
   

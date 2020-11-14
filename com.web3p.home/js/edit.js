4b65798d53ba09de333579f1ce347f72d0e90a52
erunapp <72942850+erunapp@users.noreply.github.com>
2020-11-15 04:36:45 +0900

    0.0.18

--- com.web3p.home/js/edit.js
+++ com.web3p.home/js/edit.js
@@ -7,7 +7,7 @@ let file = Number(location.search.substr(pos));
 CodeMirror(document.getElementById('code'), {
     lineNumbers: true,
     tabSize: 4,
-    value: wdp.getFile(file),
+    value: wdp.file(file),
     theme: 'cobalt'
   });
   

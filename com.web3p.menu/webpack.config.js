3353319bcfd9b26ae8b2c29c966c69d7226566c1
erunapp <web3p.pt@gmail.com>
2021-11-30 07:11:29 +0900

    0.1.0

--- com.web3p.menu/webpack.config.js
+++ com.web3p.menu/webpack.config.js
@@ -5,22 +5,30 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   target: ['web', 'es6'],
-  context: path.resolve(__dirname, 'public/src'),
+  context: path.resolve(__dirname, 'web/src'),
   entry: {
     menu: './menu/menu.js'
   },
   output: {
-    path: path.resolve(__dirname, 'public/assets/js'),
+    path: path.resolve(__dirname, 'web/assets/js'),
     filename: pathData => '[name].js'
   },
   module: {
-    rules: [{
-      test: /\.htm$/i,
-      exclude: /node_modules/,
-      loader: "html-loader",
-      options: {
-        sources: false, // Disables attributes processing
+    rules: [
+      {
+        test: /\.htm$/i,
+        exclude: /node_modules/,
+        loader: "html-loader",
+        options: {
+          sources: false, // Disables attributes processing
+        },
       },
-    }, ]
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'web'),
+    },
+    port: 8080,
   },
 };

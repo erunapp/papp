10bd87c4b742481ab31c927950bdbccb88da1431
erunapp <72942850+erunapp@users.noreply.github.com>
2021-12-16 22:26:13 +0900

    0.1.3

--- com.web3p.menu/web/src/menu.js
+++
@@ -1,6 +0,0 @@
-import './commons/init';
-import './menu/breadcrumb';
-import './menu/menu-page';
-import './menu/menu-list';
-import './menu/menu-flex';
-import './menu/menu-card';
--- com.web3p.menu/web/src/tree.js
+++
@@ -1,4 +0,0 @@
-import './commons/init.js';
-import './tree/tree-view.js';
-import './tree/tree-list.js';
-import './tree/tree-item.js';

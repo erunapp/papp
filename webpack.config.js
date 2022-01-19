818239b53a9f55e1a743383ebb9bb66582507609
erunapp <72942850+erunapp@users.noreply.github.com>
2022-01-19 18:53:20 +0900

    0.1.7

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
 'use strict';
-
 const path = require('path');
 
 module.exports = {
   mode: 'development',
   target: ['web', 'es6'],
-  context: path.resolve(__dirname, 'web/src'),
+
+  context: path.resolve(__dirname, 'com.web3p.file', 'src'),
   entry: {
-    menu: './menu.js',
-    tree: './tree.js'
+//  menu: './menu.js',
+//  tree: './tree.js',
+    file: './file.js',
   },
   output: {
-    path: path.resolve(__dirname, 'web/assets/js'),
-    filename: pathData => '[name].js'
+    path: path.resolve(__dirname, 'com.web3p.file', 'web', 'js'),
+    filename: '[name].js',
   },
+
   module: {
     rules: [
       {
         test: /\.htm$/i,
-        exclude: /node_modules/,
-        loader: "html-loader",
-        options: {
-          sources: false, // Disables attributes processing
-        },
+        type: 'asset/source',
       },
     ]
   },
+
+  resolve: {
+    alias: {
+      commons: path.resolve(__dirname, 'commons'),
+    },
+  },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'web'),
+      directory: path.join(__dirname, '.'),
     },
-    port: 8080,
+    port: 8085,
   },
 };
+// [name]: './[name].js',
+// debug: node --inspect-brk node_modules/webpack/bin/webpack.js
